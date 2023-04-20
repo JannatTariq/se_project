f7b6f01ab9ee0812e91250f45c25093de6f21699
Samina <saminabashir.cs@gmail.com>
2023-04-20 10:32:46 +0500

    Add Category

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,13 @@
 //All API requests related to products
 const express=require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createReview, getAllReview, deleteReview } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createReview, getAllReview, deleteReview, getAdminProduct  } = require("../controllers/productController");
 const {isAuthenticatedUser, authorizedRoles} = require("../middleware/authentication")
 const router=express.Router();
 
 
 router.route("/products").get(getAllProducts)
 router.route("/admin/products/new").post(isAuthenticatedUser, authorizedRoles("admin") , createProduct)
+router.route("/admin/products").get(isAuthenticatedUser,authorizedRoles("admin"),getAdminProduct);
 router.route("/admin/products/:id").put(isAuthenticatedUser, authorizedRoles("admin") ,  updateProduct).delete(isAuthenticatedUser, authorizedRoles("admin") ,  deleteProduct)
 router.route("/products/:id").get(getProductDetails)
 router.route("/review").put(isAuthenticatedUser, createReview)
